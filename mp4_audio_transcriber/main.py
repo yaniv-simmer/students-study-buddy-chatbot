639cf48672b5d93228077af2249f4f3858d90cf8
yaniv-simmer <simmeryaniv@gmail.com>
2024-09-24 15:30:37 +0300

    changed workflow

--- mp4_audio_transcriber/main.py
+++ mp4_audio_transcriber/main.py
@@ -1,9 +1,13 @@
+'''
+This script requires GPU to run(at a satisfiable speed). I couldn't run it on my local machine so I ran it on Google Colab.
+The Colab notebook is available at: https://colab.research.google.com/drive/1mxS2cXstUGyaS3a-VQssKo_y-VbSdaz9?usp=sharing
+'''
+
 import json
 import os
 from time import sleep
 
-from chunking_manager import ChunkingManager
-from mp4_audio_transcriber import MP4AudioTranscriber
+from mp4_transcriber import MP4AudioTranscriber
 
 SLEEP_TIME = 60
 MP4_UPLOADS_FOLDER = '/shared_folder/mp4_uploads/'
@@ -13,19 +17,11 @@ PROCESSED_OUTPUT_FOLDER = '/shared_folder/chunked_transcriptions/'
 
 if __name__ == '__main__':
     mp4_transcriber = MP4AudioTranscriber()
-    chunking_manager = ChunkingManager()
     while True:
         for file in os.listdir(MP4_UPLOADS_FOLDER):        
-            segments = mp4_transcriber.run(MP4_UPLOADS_FOLDER + file, RAW_OUTPUT_FOLDER)
-            chunked_segments = chunking_manager.chunk_text(segments)
-            os.remove(MP4_UPLOADS_FOLDER + 'mp4_files/' + file)
+            segments = mp4_transcriber.run(MP4_UPLOADS_FOLDER + file, RAW_OUTPUT_FOLDER + file[:-4] + '_Ivrit.json')
+            os.remove(MP4_UPLOADS_FOLDER + file)
         
-        for file in os.listdir(RAW_OUTPUT_FOLDER):
-            if file.endswith('.json') and file not in os.listdir(PROCESSED_OUTPUT_FOLDER):
-                with open(RAW_OUTPUT_FOLDER + file, 'r', encoding='utf-8') as json_file:
-                    data = json.load(json_file)
-                chunked_data = chunking_manager.chunk_text(,data)
-
         sleep(SLEEP_TIME)
                 
 
