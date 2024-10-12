1f23185a471e321212b5a5ef987064ee369a1582
yaniv-simmer <simmeryaniv@gmail.com>
2024-10-12 20:52:32 +0300

    changed some var names

--- mp4_audio_transcriber/mp4_transcriber.py
+++ mp4_audio_transcriber/mp4_transcriber.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass, asdict
 import os
 import json
-import argparse
-from typing import List, Dict
+import typing  as t
 from moviepy.editor import VideoFileClip
 import torch
 from transformers import pipeline
@@ -9,28 +9,51 @@ from transformers import pipeline
 
 CHUNK_LENGTH_SEC = 30
 
+@dataclass
+class TranscribedSegment:
+    offset_start: str
+    offset_end: str
+    text: str
+    lang: str
+    media_type: str
+    ref: str
+    course_name: str
+
+    def to_dict(self):
+        return asdict(self)
+
+
 class MP4AudioTranscriber:
     '''
     Class to transcribe audio from an mp4 file using the Ivrit-ai whisper model. 
-    For more info: https://huggingface.co/ivrit-ai/whisper-v2-d3-e3
+    For more info about Ivrit-ai: https://huggingface.co/ivrit-ai/whisper-v2-d3-e3
     '''
+
     def __init__(self):
-        self.mp4_path = None
-        self.output_path = None
-        self.temp_audio_path = None
-        self.url = None
+        '''
+        Initialize the pipeline for automatic speech recognition using the Ivrit-ai whisper model.
+
+        Attributes:
+            device (str): Device to use for the pipeline (cuda:0 if GPU is available, else cpu).
+            pipe (AutomaticSpeechRecognitionPipeline): Transformers library pipeline for automatic speech recognition.
+        '''
+        #TODO: checkout whispher fast model
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.pipe = pipeline(
-            "automatic-speech-recognition",
-            model="ivrit-ai/whisper-v2-d3-e3",
-            chunk_length_s=CHUNK_LENGTH_SEC,
-            device=self.device,
-        )
+                        "automatic-speech-recognition",
+                        model="ivrit-ai/whisper-v2-d3-e3",
+                        chunk_length_s=CHUNK_LENGTH_SEC,
+                        device=self.device,
+                        )
 
-    def extract_audio(self) -> None:
-        '''Extract audio from the mp4 file'''
-        video = VideoFileClip(self.mp4_path)
-        video.audio.write_audiofile(self.temp_audio_path, codec='pcm_s16le')
+
+    def extract_audio(self, mp4_path: str) -> str:
+        ''' Extract audio from the mp4 file into a temporary WAV file, and return the path to the WAV file. '''
+        temp_wav_audio_path = mp4_path[:-4] + 'temp_audio.wav'
+        video = VideoFileClip(mp4_path)
+        video.audio.write_audiofile(temp_wav_audio_path, codec='pcm_s16le')
+        return temp_wav_audio_path
+
 
     def format_time(self, seconds: float) -> str:
         '''Convert seconds to HH:MM:SS format'''
@@ -39,37 +62,44 @@ class MP4AudioTranscriber:
         secs = seconds % 60
         return f"{hours}:{minutes:02d}:{secs:06.3f}"
 
-    def transcribe_audio(self) -> List[Dict[str, str]]:
+
+    def transcribe_audio(self, temp_wav_audio_path: str, url: str, course_name: str) -> t.List[TranscribedSegment]:
         '''
         Transcribe the audio file and return the transcribed segments. 
 
+        Args:
+            temp_wav_audio_path (str): Path to the temporary wav audio file.
+            url (str): URL of the mp4 file (for reference).
+            course_name (str): Name of the course.
+
         Returns:
-            List[Dict[str, str]]: List of transcribed segments with the following keys:
-                'offset_start', 'offset_end', 'text', 'lang', 'type', 'ref'.
+            List[TranscribedSegment]: List of transcribed segments.
         '''
-        prediction = self.pipe(self.temp_audio_path, batch_size=8, return_timestamps=True)
+        prediction = self.pipe(temp_wav_audio_path, batch_size=8, return_timestamps=True)
         segments = []
 
         for chunk in prediction.get('chunks', []):
-            start_time, end_time = chunk['timestamp']
-            segments.append({
-                "offset_start": self.format_time(start_time),
-                "offset_end": self.format_time(end_time),
-                "text": chunk['text'].strip(),
-                "lang": "he", #TODO: add language detection
-                "type": "audio",
-                "ref": self.url
-            })
-
+            start_time, end_time = chunk['timestamp']         
+            segments.append(TranscribedSegment(
+                                            offset_start=self.format_time(start_time),
+                                            offset_end=self.format_time(end_time),
+                                            text=chunk['text'],
+                                            lang='he',
+                                            media_type='audio',
+                                            ref=url,
+                                            course_name=course_name
+                                            ))
         return segments
 
 
-    def save_json(self, data: List[Dict[str, str]]) -> None:
+    def save_json(self, data: t.List[TranscribedSegment], output_path: str) -> None:
         '''Save the transcribed segments to a JSON file'''
-        with open(self.output_path, 'w', encoding='utf-8') as json_file:
-            json.dump({"data": data}, json_file, ensure_ascii=False, indent=2)
+        formatted_data = [transcrip.to_dict() for transcrip in data]
+        with open(output_path, 'w', encoding='utf-8') as json_file:
+            json.dump({"data": formatted_data}, json_file, ensure_ascii=False, indent=2)
 
-    def run(self, mp4_path: str, output_path: str, url: str) -> List[Dict[str, str]]:
+
+    def run(self, mp4_path: str, output_path: str, url: str, course_name: str) -> None:
         '''
         Main function to transcribe audio from an mp4 file and save the transcription to a JSON file.
 
@@ -77,28 +107,15 @@ class MP4AudioTranscriber:
             mp4_path (str): Path to the mp4 file.
             output_path (str): Path to the output JSON file.
             url (str): URL of the mp4 file (for reference).
+            course_name (str): Name of the course.
 
-        Returns:
-            List[Dict[str, str]]: List of transcribed segments with the following keys:
-                'offset_start', 'offset_end', 'text', 'lang', 'type', 'ref'.
+        Saves:
+            JSON file containing the transcribed segments.
         '''
-        self.mp4_path =  mp4_path
-        self.output_path = output_path
-        self.temp_audio_path = mp4_path[:-4] + 'temp_audio.wav'
-        self.url = url
+        temp_wav_audio_path = self.extract_audio(mp4_path)
+        segments = self.transcribe_audio(temp_wav_audio_path, url, course_name)
+        self.save_json(segments, output_path)
 
-        print("Extracting audio...")
-        self.extract_audio()
-
-        print("Transcribing audio...")
-        segments = self.transcribe_audio()
-
-        print("Saving transcription to JSON...")
-        self.save_json(segments)
-
-        os.remove(self.temp_audio_path)
-        print("Done.")
-
-        return segments
-
-
+        os.remove(mp4_path)
+        os.remove(temp_wav_audio_path)
+        
