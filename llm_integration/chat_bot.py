d79a3eeb9db64ebb152ff4eabe93a70b8218642d
yaniv-simmer <simmeryaniv@gmail.com>
2024-10-20 23:41:41 +0300

    added gemini config option

--- llm_integration/chat_bot.py
+++ llm_integration/chat_bot.py
@@ -1,191 +1,90 @@
-import json
-import time
-from langchain.llms import HuggingFaceHub
-from typing import List, Dict, Any, Tuple
-from langchain import PromptTemplate
-from dotenv import load_dotenv
-import os
-
 import requests
+import json
+import os
+import typing as t
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.llms import HuggingFaceHub
+from langchain import PromptTemplate
+
 load_dotenv()
 
-URL = 'http://localhost:5001/search'
-
-
-
-
-class LLMHandler:
-    def __init__(self, llm_model_name: str, huggingface_model_kwargs: Dict[str, Any] = {}):
-        self.llm = self.initialize_llm(llm_model_name, huggingface_model_kwargs)
-
-    def initialize_llm(self, llm_model_name, huggingface_model_kwargs):
-        
-        huggingface_api_key = os.environ.get('HUGGINGFACE_API_KEY')
-        
-        return HuggingFaceHub(
-            repo_id=llm_model_name,
-            model_kwargs=huggingface_model_kwargs,
-            huggingfacehub_api_token=huggingface_api_key
-        )
-
-    def generate_answer(self, prompt_text: str) -> str:
-        return self.llm(prompt_text)
-    
-
-
-# **PromptManager** class
-class PromptManager:
-    def __init__(self, language_config: str):
-        self.template = self.create_prompt_template(language_config)
-
-    def create_prompt_template(self, language_config) -> PromptTemplate:
-        if language_config == 'english':
-            template = config.get('prompt_template', """
-You are a lecturer. The user will ask you questions. Use the following context to answer the question.
-If you don't know the answer, just say you don't know.
-No longer than 2 sentences.
-
-Context: {context}
-
-Question: {question}
-
-""")
-        else:
-            template = config.get('prompt_template', """
-אתה מרצה באוניברסיטה. המשתמש ישאל אותך שאלות. השתמש רק באחד מההקשרים הבאים כדי לענות על השאלה.
-אם אינך יודע את התשובה, פשוט אמור שאינך יודע.
-ספק תשובה קצרה ותמציתית, לא יותר מ-2 משפטים.
-
-
-הקשר:
-{context}
-
-השאלה של המשתמש: 
-{question}
-
-""")
-        
-        return PromptTemplate(template=template, input_variables=["context", "question"])
-
-    def format_prompt(self, context: str, question: str) -> str:
-        return self.template.format(context=context, question=question)
-
+CONFIG_INDEX = 1
+DB_SEARCH_ENDPOINT = 'http://localhost:5001/similarity_search_with_score'
+PROMPT_TEMPLATE = (
+    "You are a lecturer. The user will ask you questions. Use the following list of contexts to answer the question."
+    "answer the question in Hebrew. No longer than 2 sentences."
+    "Also refer to the context that was most relavent to the question. the last character should be the contex number that was most relavent"
+    "\nContext: {context}"
+    "\nQuestion: {question}"
+)
 
 
 class ChatBot:
     '''
     A class to represent the ChatBot.
-
     '''
-    #TODO: the chatbot should be able to keep running while new data is being added to the vector store
-    #TODO: the chatbot should warn the user if the answer is not accurate enough
-    #TODO: try milvus 
-    #TODO: check if llm can get a 'context' parameter? whats Q&A?
-    #TODO: use radas !!
-    def __init__(self, llm_model_name: str, language_config: str, model_kwargs: Dict[str, Any] = {}):
-        self.llm_handler = LLMHandler(llm_model_name, model_kwargs)
-        self.prompt_manager = PromptManager(language_config)
-    
-    def answer_question(self, question: str) -> Tuple[str, Dict[str, List[str]], float]:
+
+    def __init__(self, enable_gemini: bool):
+        self.llm = self.initialize_llm(enable_gemini)
+        self.prompt_template = PromptTemplate(template=PROMPT_TEMPLATE,
+                                              input_variables=["context", "question"]
+                                              )
+
+    def initialize_llm(self, enable_gemini: bool) -> t.Union[HuggingFaceHub, ChatGoogleGenerativeAI]:
+        ''' Initialize the LLM model based on the configuration file or the gemini-1.5-flash model. '''
+        if enable_gemini:
+            return ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                                          temperature=0.1,
+                                          max_tokens=None,
+                                          timeout=None,
+                                          max_retries=2,
+                                          )
+        else:
+            configs_lst = self.load_config_from_file()
+            config = configs_lst[CONFIG_INDEX]
+            return HuggingFaceHub(repo_id=config["llm_model_name"],
+                                  model_kwargs=config["huggingface_model_kwargs"],
+                                  huggingfacehub_api_token=os.environ.get('HUGGINGFACE_API_KEY')
+                                  )
+
+    def answer_question(self, query: str, course_name: str) -> t.Tuple[str, t.List[t.Dict]]:
         '''
-        Answer a user question and return the answer, metadata info, and elapsed time.
+        Answer a question based on the user query and the course name.
 
         Args:
-            question (str): The user's question.
+            query (str): The user query.
+            course_name (str): The course name.
+            k (int): The number of results to return.
 
         Returns:
-            str: The LLM answer to the user's question.
-            Dict[str, List[Union[Dict[str, Any], str]]]: Metadata information of the 3 sources with the highest similarity scores.
-                - The dictionary keys represent the rank of each document based on the similarity score (e.g., "1", "2", "3").
-                - Each key maps to a list of three elements:
-                    1. A dictionary (`Dict[str, Any]`) containing the metadata for the document. This typically includes:
-                        - 'type': The type of the document (e.g., "pdf", "text", "web").
-                        - 'ref': A reference to the document, such as a URL, document ID, or file path.
-                        - Any other metadata fields associated with the document, depending on the source.
-                    2. A string representing the accuracy or similarity score, formatted as "accuracy: <normalized_score>".
-                    3. The page content (string) extracted from the document, processed and formatted for display (e.g., a summary or excerpt).
-
-            float: The elapsed time for the process, in seconds.
+            tuple[str, list[dict]]: The answer and the retrieved data
+                str: The answer to the question.
+                list[dict]: The retrieved data from the database.
+                                dict: {
+                                    "page_content": str,
+                                    "metadata": dict,
+                                    "score": float
+                                }
         '''
-        start_time = time.time()
-
-        data = {'query': question, 'k': 3}
-        
-        response = requests.post(URL, json=data)
+        response = requests.get(DB_SEARCH_ENDPOINT, params={'query': query, "course_name": course_name})
         if response.status_code != 200:
-            return f"An error occurred: {response.text}", {}, 0.0
+            return f"An error occurred: {response.text}", {}
         response_data = response.json()
-        context = response_data['context']
-        metadata_info = response_data['metadata']        
-        
 
-        prompt_text = self.prompt_manager.format_prompt(context=context, question=question)
+        db_data = response_data['docs_and_scores']
+        formatted_prompt = self.format_prompt(db_data, query)
+        answer = self.llm.invoke(formatted_prompt)
+        return answer, db_data
 
-        answer = self.llm_handler.generate_answer(prompt_text)
+    def format_prompt(self, db_data: str, question: str) -> str:
+        ''' Format the prompt based on the retrieved data and the question. '''
+        retrived_context = [f"{i + 1}.    {doc['page_content']}" for i, doc in enumerate(db_data)]
+        context = '\n\n'.join(retrived_context)
+        return self.prompt_template.format(context=context, question=question)
 
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        
-        return answer, metadata_info, elapsed_time
-      
-
-    def normalize_cosine_similarity(self, cosine_similarity: float) -> float:
-        return (cosine_similarity + 1) / 2
-
-
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-# Initialize the ChatBot instance
-
-CONFIG_FILE = 'llm_integration/config.json'
-CONFIG_INDEX = 0
-
-def load_config_from_file(config_file: str) -> dict:
-    with open(config_file, 'r') as file:
-        return json.load(file)["configs"][CONFIG_INDEX]
-    
-config = load_config_from_file(CONFIG_FILE)
-
-chatbot = ChatBot(llm_model_name=config['llm_model_name'], language_config=config['language'],
-                   model_kwargs=config['huggingface_model_kwargs'])
-
-@app.route('/api/answer', methods=['POST'])
-def answer_question():
-    """
-    Flask API endpoint to get an answer from the chatbot.
-
-    The request should contain a JSON body with a "question" field:
-    {
-        "question": "What is your question?"
-    }
-
-    The response will return the chatbot's answer, metadata, and elapsed time in JSON format:
-    {
-        "answer": "Chatbot's answer",
-        "metadata": { ... },  # Metadata information of the top documents
-        "elapsed_time": 1.23   # Time taken to process the request
-    }
-    """
-    # Extract the user question from the request
-    data = request.get_json()
-    question = data.get('question')
-
-    if not question:
-        return jsonify({"error": "Please provide a question."}), 400
-
-    # Get the answer, metadata info, and elapsed time from the chatbot
-    answer, metadata_info, elapsed_time = chatbot.answer_question(question)
-
-    # Return the result as a JSON response
-    return jsonify({
-        "answer": answer,
-        "metadata": metadata_info,
-        "elapsed_time": elapsed_time
-    })
-
-
-if __name__ == '__main__':
-    # Run the Flask app
-    app.run(debug=True)
+    def load_config_from_file(self, config_file: str) -> t.Dict[str, str]:
+        ''' Load the configuration from the configuration file and return a list of optional configs'''
+        with open(config_file, 'r') as file:
+            return json.load(file)["configs"]
